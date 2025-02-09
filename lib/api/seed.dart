5bb0c0f10ea7772b3e097c37f85ac0484ecc6663
S.a. Sadik <sadik5397@gmail.com>
2025-02-09 17:05:39 +0600

    Update seed.dart

--- lib/api/seed.dart
+++ lib/api/seed.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:aosed_survey/service/global_function.dart';
+
 class Seed {
   static List<String> example = ["Lorem", "Ipsum", "Dolor", "Set"];
   static List<String> randomNames = [
@@ -51,6 +53,17 @@ class Seed {
     'Hong Wu',
     'Jin-ho Lee'
   ];
+
   static List<String> randomNameList(int count) => (List<String>.from(randomNames)..shuffle(Random())).take(count).toList();
   static String randomName = randomNames[Random().nextInt(randomNames.length)];
+
+  static String randomString(int len) => generateRandomString(len);
+
+  static String randomNumber(int len) => generateRandomNumber(len);
+
+  static String randomAvatarUrl([int? size]) => "https://i.pravatar.cc/${size ?? 150}?u=${generateRandomNumber(4)}";
+
+  static String randomImageUrl([int? height, int? width]) => "https://picsum.photos/${height ?? 512}/${width ?? 512}?random=${generateRandomNumber(4)}";
+
+  static List<Map> randomObjects = List.generate(randomNames.length, (index) => {'id': index + 1, 'value': randomNames[index], 'title': randomNames[index]})..shuffle();
 }
