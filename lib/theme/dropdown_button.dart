eed56c38af6852d123d38d21366040f89589cc6f
S.a. Sadik <sadik5397@gmail.com>
2025-02-10 07:25:02 +0600

    Update dropdown_button.dart

--- lib/theme/dropdown_button.dart
+++ lib/theme/dropdown_button.dart
@@ -6,13 +6,16 @@ import '../theme/padding.dart';
 
 class ThemeDropDown {
   //region private props
-  static Duration _searchDelay = Duration(milliseconds: 300);
+  static final Duration _searchDelay = const Duration(milliseconds: 300);
 
   static MenuProps _dropDownMenuDecoration() => MenuProps(borderRadius: ThemeBorderRadius.r2);
 
   static Padding _optionsTitle({required String title}) => Padding(padding: ThemePadding.p4.copyWith(bottom: ThemePadding.value), child: Text("Select $title", textAlign: TextAlign.center));
 
-  static TextFieldProps _searchField({required TextEditingController search}) => TextFieldProps(autofocus: true, controller: search, maxLines: 1, decoration: _searchTextFiledDecoration());
+  static TextFieldProps _searchField() {
+    TextEditingController searchController = TextEditingController();
+    return TextFieldProps(autofocus: true, controller: searchController, maxLines: 1, decoration: _searchTextFiledDecoration());
+  }
 
   static InputDecoration _searchTextFiledDecoration() => InputDecoration(
       focusedBorder: OutlineInputBorder(borderRadius: ThemeBorderRadius.r1, borderSide: BorderSide(color: ThemeColor.primary.withValues(alpha: .3), width: 1)),
@@ -51,27 +54,32 @@ class ThemeDropDown {
 
   static DropdownButtonProps _dropDownIcon() => DropdownButtonProps(iconSize: 18, icon: Icon(Icons.keyboard_arrow_down), color: ThemeColor.jetBlack.withValues(alpha: .4));
 
+  static ClearButtonProps _clearButton(bool showClearButton) => ClearButtonProps(isVisible: showClearButton, iconSize: 18, icon: Icon(Icons.clear), color: ThemeColor.jetBlack.withValues(alpha: .4));
+
+  static Text _dropDownValues(List selectedItems, {String? itemString}) =>
+      Text(itemString == null ? selectedItems.join(", ") : (List.generate(selectedItems.length, (index) => selectedItems[index][itemString])).join(", "),
+          style: TextStyle(color: ThemeColor.jetBlack, fontSize: 14, fontWeight: FontWeight.w400), maxLines: 1, overflow: TextOverflow.ellipsis);
+
   static DropDownDecoratorProps _decoratorProps({required String title, String? errorText, required bool isDisable}) => DropDownDecoratorProps(
       textAlignVertical: TextAlignVertical.center,
       dropdownSearchDecoration: _dropDownDecoration(title: title, enabled: !isDisable, errorText: errorText),
       baseStyle: TextStyle(color: ThemeColor.jetBlack, fontSize: 14, fontWeight: FontWeight.w400));
 
   static PopupPropsMultiSelection<dynamic> _multiSelectionMap({required String itemString, required String title, required bool useSearch}) {
-    TextEditingController search = TextEditingController();
     return PopupPropsMultiSelection.menu(
         constraints: const BoxConstraints(maxHeight: 600, maxWidth: 300),
+        showSelectedItems: true,
         itemBuilder: (context, item, isSelected) => _itemDesign(item: item[itemString], isSelected: isSelected),
         menuProps: _dropDownMenuDecoration(),
         searchDelay: _searchDelay,
         // disabledItemFn: (String s) => s.startsWith('I'),
         showSearchBox: useSearch,
         fit: FlexFit.loose,
-        searchFieldProps: _searchField(search: search),
+        searchFieldProps: _searchField(),
         title: _optionsTitle(title: title));
   }
 
   static PopupPropsMultiSelection<String> _multiSelectionString({required String title, required bool useSearch}) {
-    TextEditingController search = TextEditingController();
     return PopupPropsMultiSelection.menu(
         constraints: const BoxConstraints(maxHeight: 600, maxWidth: 300),
         showSelectedItems: true,
@@ -81,12 +89,11 @@ class ThemeDropDown {
         // disabledItemFn: (String s) => s.startsWith('I'),
         showSearchBox: useSearch,
         fit: FlexFit.loose,
-        searchFieldProps: _searchField(search: search),
+        searchFieldProps: _searchField(),
         title: _optionsTitle(title: title));
   }
 
-  static PopupProps<dynamic> _singleSelectionMap({required String itemString, required String title, required bool useSearch}) {
-    TextEditingController search = TextEditingController();
+  static PopupProps<dynamic> _singleSelectionMap({required String title, required String itemString, required bool useSearch}) {
     return PopupProps.menu(
         constraints: const BoxConstraints(maxHeight: 600, maxWidth: 300),
         showSelectedItems: true,
@@ -96,12 +103,11 @@ class ThemeDropDown {
         // disabledItemFn: (String s) => s.startsWith('I'),
         showSearchBox: useSearch,
         fit: FlexFit.loose,
-        searchFieldProps: _searchField(search: search),
+        searchFieldProps: _searchField(),
         title: _optionsTitle(title: title));
   }
 
   static PopupProps<String> _singleSelectionString({required String title, required bool useSearch}) {
-    TextEditingController search = TextEditingController();
     return PopupProps.menu(
         constraints: const BoxConstraints(maxHeight: 600, maxWidth: 300),
         showSelectedItems: true,
@@ -111,105 +117,133 @@ class ThemeDropDown {
         // disabledItemFn: (String s) => s.startsWith('I'),
         showSearchBox: useSearch,
         fit: FlexFit.loose,
-        searchFieldProps: _searchField(search: search),
+        searchFieldProps: _searchField(),
         title: _optionsTitle(title: title));
   }
 
 //endregion
 
-  static AbsorbPointer primary(
+  static DropdownSearch primary(
       {bool useSearch = true,
       isDisable = false,
       bool Function(String item, String filter)? filterFn,
+      bool showClearButton = true,
       double? width,
       required String title,
       String? errorText,
       required List<String> options,
       required dynamic value,
       required void Function(Object? value) onChanged}) {
-    return AbsorbPointer(
-        absorbing: isDisable,
-        child: DropdownSearch<String>(
-            items: options,
-            filterFn: filterFn,
-            selectedItem: value,
-            onChanged: onChanged,
-            enabled: !isDisable,
-            dropdownButtonProps: _dropDownIcon(),
-            popupProps: _singleSelectionString(title: title, useSearch: useSearch),
-            dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable)));
+    return DropdownSearch<String>(
+        items: options,
+        filterFn: filterFn,
+        selectedItem: value,
+        onChanged: onChanged,
+        enabled: !isDisable,
+        dropdownButtonProps: _dropDownIcon(),
+        popupProps: _singleSelectionString(title: title, useSearch: useSearch),
+        clearButtonProps: _clearButton(showClearButton),
+        dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable));
   }
 
-  static AbsorbPointer map(
+  static DropdownSearch map(
       {bool useSearch = true,
       isDisable = false,
       bool Function(dynamic item, String filter)? filterFn,
+      bool showClearButton = true,
       double? width,
       required String title,
-      itemString,
+      required String itemString,
       String? errorText,
-      required List<String> options,
+      required List<dynamic> options,
       required dynamic value,
       required void Function(Object? value) onChanged}) {
-    return AbsorbPointer(
-        absorbing: isDisable,
-        child: DropdownSearch<dynamic>(
-            items: options,
-            itemAsString: (item) => item[itemString],
-            filterFn: filterFn,
-            selectedItem: value,
-            onChanged: onChanged,
-            enabled: !isDisable,
-            dropdownButtonProps: _dropDownIcon(),
-            popupProps: _singleSelectionMap(title: title, useSearch: useSearch, itemString: itemString),
-            dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable)));
+    return DropdownSearch<dynamic>(
+        items: options.where((option) => option != null).toList(),
+        itemAsString: (item) => item[itemString],
+        compareFn: (item, selectedItem) => item == null ? false : (item[itemString] == selectedItem[itemString]),
+        filterFn: filterFn,
+        selectedItem: value,
+        onChanged: onChanged,
+        enabled: !isDisable,
+        popupProps: _singleSelectionMap(title: title, useSearch: useSearch, itemString: itemString),
+        dropdownButtonProps: _dropDownIcon(),
+        clearButtonProps: _clearButton(showClearButton),
+        dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable));
   }
 
-  static AbsorbPointer multiSelect(
+  static DropdownSearch multiSelect(
       {bool useSearch = true,
       isDisable = false,
       bool Function(String item, String filter)? filterFn,
+      bool showClearButton = true,
       double? width,
       required String title,
       String? errorText,
       required List<String> options,
-      values,
+      List<String>? values,
       required void Function(Object? value) onChanged}) {
-    return AbsorbPointer(
-        absorbing: isDisable,
-        child: DropdownSearch<String>.multiSelection(
+    return (values?.isEmpty ?? true)
+        ? DropdownSearch<String>.multiSelection(
             items: options,
             filterFn: filterFn,
-            selectedItems: values,
+            selectedItems: [],
             onChanged: onChanged,
             enabled: !isDisable,
             dropdownButtonProps: _dropDownIcon(),
             popupProps: _multiSelectionString(title: title, useSearch: useSearch),
-            dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable)));
+            clearButtonProps: _clearButton(showClearButton),
+            dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable))
+        : DropdownSearch<String>.multiSelection(
+            dropdownBuilder: (context, selectedItems) => _dropDownValues(selectedItems),
+            items: options,
+            filterFn: filterFn,
+            selectedItems: values!,
+            onChanged: onChanged,
+            enabled: !isDisable,
+            dropdownButtonProps: _dropDownIcon(),
+            popupProps: _multiSelectionString(title: title, useSearch: useSearch),
+            clearButtonProps: _clearButton(showClearButton),
+            dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable));
   }
 
-  static AbsorbPointer multiSelectMap(
+  static DropdownSearch multiSelectMap(
       {bool useSearch = true,
       isDisable = false,
       bool Function(dynamic item, String filter)? filterFn,
+      bool showClearButton = true,
       double? width,
       required String title,
-      itemString,
+      required String itemString,
       String? errorText,
-      required List<String> options,
-      values,
+      required List<dynamic> options,
+      List<String>? values,
       required void Function(Object? value) onChanged}) {
-    return AbsorbPointer(
-        absorbing: isDisable,
-        child: DropdownSearch<dynamic>.multiSelection(
-            items: options,
+    return (values?.isEmpty ?? true)
+        ? DropdownSearch<dynamic>.multiSelection(
+            items: options.where((option) => option != null).toList(),
             itemAsString: (item) => item[itemString],
+            compareFn: (item, selectedItem) => item == null ? false : (item[itemString] == selectedItem[itemString]),
             filterFn: filterFn,
-            selectedItems: values,
+            selectedItems: [],
             onChanged: onChanged,
             enabled: !isDisable,
             dropdownButtonProps: _dropDownIcon(),
             popupProps: _multiSelectionMap(itemString: itemString, title: title, useSearch: useSearch),
-            dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable)));
+            clearButtonProps: _clearButton(showClearButton),
+            dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable))
+        : DropdownSearch<dynamic>.multiSelection(
+            dropdownBuilder: (context, selectedItems) => _dropDownValues(selectedItems, itemString: itemString),
+            items: options.where((option) => option != null).toList(),
+            itemAsString: (item) => item[itemString],
+            compareFn: (item, selectedItem) => item == null ? false : (item[itemString] == selectedItem[itemString]),
+            filterFn: filterFn,
+            selectedItems: values!,
+            onChanged: onChanged,
+            enabled: !isDisable,
+            dropdownButtonProps: _dropDownIcon(),
+            popupProps: _multiSelectionMap(itemString: itemString, title: title, useSearch: useSearch),
+            clearButtonProps: _clearButton(showClearButton),
+            dropdownDecoratorProps: _decoratorProps(title: title, errorText: errorText, isDisable: isDisable));
   }
 }
